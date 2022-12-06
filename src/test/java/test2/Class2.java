0addc95405c78ddadc960ada0c0ef4c49b8d641c
farhankhalid08 <farhan.khalid@hotmail.co.uk>
2022-12-05 23:04:14 -0500

    Project 2

---
+++ src/test/java/test2/Class2.java
@@ -0,0 +1,35 @@
+package test2;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Class2 {
+	public static WebDriver driver;
+
+	
+	@Test
+	public void Alert() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://rediff.com");
+		
+		driver.findElement(By.cssSelector("a.signin")).click();
+		driver.findElement(By.cssSelector("input.signinbtn")).click();
+		Alert alert= driver.switchTo().alert();
+		String text = alert.getText();
+		System.out.println(text);
+		if(text.equals("Please enter a valid user name")) {
+			alert.accept();
+		}else {
+			alert.dismiss();
+		}
+         driver.findElement(By.id("login1")).sendKeys("Farhankhalid920@hotmail.com");
+         driver.findElement(By.id("password")).sendKeys("subhan");
+         driver.quit();
+		
+	}
+
+}
